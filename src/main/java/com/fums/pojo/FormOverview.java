c7673fc90915c0fa1041fab9d3772647ab043d19
Administrator <@huangdebao>
2020-07-17 17:53:58 +0800

    717提交

---
+++ src/main/java/com/fums/pojo/FormOverview.java
@@ -0,0 +1,192 @@
+package com.fums.pojo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fums.system_management.pojo.FollowUp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * form_overview
+ * @author 
+ */
+public class FormOverview implements Serializable {
+    private Integer overviewId;
+
+    /**
+     * 科室id
+     */
+    private Integer departmentId;
+
+    /**
+     * 随访组ID
+     */
+    private Integer followupUpId;
+
+    /**
+     * 模板名称
+     */
+    private String templateName;
+
+    /**
+     * 更新日期
+     */
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date updatedDate;
+
+    /**
+     * 更新者
+     */
+    private String updateThe;
+
+    /**
+     * 版本号
+     */
+    private String theErsionUmber;
+
+    /**
+     * 创建者
+     */
+    private String theReator;
+
+    /**
+     * 创建日期
+     */
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date creationDate;
+
+    /**
+     * 状态（0：历史版本 1：当前版本 2；未发布）
+     */
+    private String state;
+
+    /**
+     * 表状态(1:删除，2:未删除)
+     */
+    private String tableState;
+
+    private FollowUp followUp;
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public String toString() {
+        return "FormOverview{" +
+                "overviewId=" + overviewId +
+                ", departmentId=" + departmentId +
+                ", followupUpId=" + followupUpId +
+                ", templateName='" + templateName + '\'' +
+                ", updatedDate=" + updatedDate +
+                ", updateThe='" + updateThe + '\'' +
+                ", theErsionUmber='" + theErsionUmber + '\'' +
+                ", theReator='" + theReator + '\'' +
+                ", creationDate=" + creationDate +
+                ", state='" + state + '\'' +
+                ", tableState='" + tableState + '\'' +
+                ", followUp=" + followUp +
+                '}';
+    }
+
+    public Integer getOverviewId() {
+        return overviewId;
+    }
+
+    public void setOverviewId(Integer overviewId) {
+        this.overviewId = overviewId;
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Integer getFollowupUpId() {
+        return followupUpId;
+    }
+
+    public void setFollowupUpId(Integer followupUpId) {
+        this.followupUpId = followupUpId;
+    }
+
+    public String getTemplateName() {
+        return templateName;
+    }
+
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public String getUpdateThe() {
+        return updateThe;
+    }
+
+    public void setUpdateThe(String updateThe) {
+        this.updateThe = updateThe;
+    }
+
+    public String getTheErsionUmber() {
+        return theErsionUmber;
+    }
+
+    public void setTheErsionUmber(String theErsionUmber) {
+        this.theErsionUmber = theErsionUmber;
+    }
+
+    public String getTheReator() {
+        return theReator;
+    }
+
+    public void setTheReator(String theReator) {
+        this.theReator = theReator;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getTableState() {
+        return tableState;
+    }
+
+    public void setTableState(String tableState) {
+        this.tableState = tableState;
+    }
+
+    public FollowUp getFollowUp() {
+        return followUp;
+    }
+
+    public void setFollowUp(FollowUp followUp) {
+        this.followUp = followUp;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+}
---
+++ src/main/java/com/fums/pojo/User.java
@@ -0,0 +1,265 @@
+package com.fums.pojo;
+
+import com.fums.system_management.pojo.HospitalDepartment;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * user
+ * @author
+ */
+public class User implements Serializable {
+    /**
+     * id
+     */
+    private Integer id;
+
+    /**
+     * 用户id
+     */
+    private Integer userId;
+
+    /**
+     * 用户昵称
+     */
+    private String userAccount;
+
+    /**
+     * 用户账号
+     */
+    private String userName;
+
+    /**
+     * 用户密码
+     */
+    private String password;
+
+    /**
+     * 用户性别
+     */
+    private String userSex;
+
+    /**
+     * 用户住址
+     */
+    private String userAddress;
+
+    /**
+     * 用户联系方式
+     */
+    private String userPhone;
+
+    /**
+     * 用户邮箱
+     */
+    private String userEmail;
+
+    /**
+     * 加密盐值
+     */
+    private String userSalt;
+
+    /**
+     * 用户状态
+     */
+    private Integer userState;
+
+    /**
+     * 科室下属科代号
+     */
+    private Integer hospitalDepartmentChildCode;
+
+    /**
+     * 用户创建时间
+     */
+    private Date userCreateTime;
+
+    /*
+     *引入科室名称
+     *
+     */
+    private String hospitalDepartmentName;
+
+    /**
+     * 用户更新时间
+     */
+    private Date userUpdateTime;
+
+    /*引入科室表*/
+    private HospitalDepartment hospitalDepartment;
+
+    private String hospitalDepartmentId;
+
+
+    /*引入用户科室中间表*/
+    private UserDepartment userDepartment;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", userAccount='" + userAccount + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", userSex='" + userSex + '\'' +
+                ", userAddress='" + userAddress + '\'' +
+                ", userPhone='" + userPhone + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                ", userSalt='" + userSalt + '\'' +
+                ", userState=" + userState +
+                ", hospitalDepartmentChildCode=" + hospitalDepartmentChildCode +
+                ", userCreateTime=" + userCreateTime +
+                ", hospitalDepartmentName='" + hospitalDepartmentName + '\'' +
+                ", userUpdateTime=" + userUpdateTime +
+                ", hospitalDepartment=" + hospitalDepartment +
+                ", hospitalDepartmentId='" + hospitalDepartmentId + '\'' +
+                ", userDepartment=" + userDepartment +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserAccount() {
+        return userAccount;
+    }
+
+    public void setUserAccount(String userAccount) {
+        this.userAccount = userAccount;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(String userSex) {
+        this.userSex = userSex;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserSalt() {
+        return userSalt;
+    }
+
+    public void setUserSalt(String userSalt) {
+        this.userSalt = userSalt;
+    }
+
+    public Integer getUserState() {
+        return userState;
+    }
+
+    public void setUserState(Integer userState) {
+        this.userState = userState;
+    }
+
+    public Integer getHospitalDepartmentChildCode() {
+        return hospitalDepartmentChildCode;
+    }
+
+    public void setHospitalDepartmentChildCode(Integer hospitalDepartmentChildCode) {
+        this.hospitalDepartmentChildCode = hospitalDepartmentChildCode;
+    }
+
+    public Date getUserCreateTime() {
+        return userCreateTime;
+    }
+
+    public void setUserCreateTime(Date userCreateTime) {
+        this.userCreateTime = userCreateTime;
+    }
+
+    public String getHospitalDepartmentName() {
+        return hospitalDepartmentName;
+    }
+
+    public void setHospitalDepartmentName(String hospitalDepartmentName) {
+        this.hospitalDepartmentName = hospitalDepartmentName;
+    }
+
+    public Date getUserUpdateTime() {
+        return userUpdateTime;
+    }
+
+    public void setUserUpdateTime(Date userUpdateTime) {
+        this.userUpdateTime = userUpdateTime;
+    }
+
+    public HospitalDepartment getHospitalDepartment() {
+        return hospitalDepartment;
+    }
+
+    public void setHospitalDepartment(HospitalDepartment hospitalDepartment) {
+        this.hospitalDepartment = hospitalDepartment;
+    }
+
+    public String getHospitalDepartmentId() {
+        return hospitalDepartmentId;
+    }
+
+    public void setHospitalDepartmentId(String hospitalDepartmentId) {
+        this.hospitalDepartmentId = hospitalDepartmentId;
+    }
+
+    public UserDepartment getUserDepartment() {
+        return userDepartment;
+    }
+
+    public void setUserDepartment(UserDepartment userDepartment) {
+        this.userDepartment = userDepartment;
+    }
+}
